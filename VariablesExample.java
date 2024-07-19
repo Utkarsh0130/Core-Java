9a4e92fbf784888559c8a7cb9d75e8ca7241be95
Utkarsh Bhutada <124357090+Utkarsh0130@users.noreply.github.com>
2024-07-19 20:24:23 +0530

    Day-1
    Basic java program for understanding variables

---
+++ VariablesExample.java
@@ -0,0 +1,13 @@
+// This program demonstrates the use of variables in Java
+public class VariablesExample {
+    public static void main(String[] args) {
+        // Declaring an integer variable
+        int number;
+
+        // Initializing the variable
+        number = 10;
+
+        // Printing the value of the variable
+        System.out.println("The value of number is: " + number);
+    }
+}
