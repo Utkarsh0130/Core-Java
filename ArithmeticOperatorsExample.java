726b1e5870648d819ef8463285f2b4b9ce30deee
Utkarsh Bhutada <124357090+Utkarsh0130@users.noreply.github.com>
2024-07-20 21:33:18 +0530

    Day-2
    This is example of the different Arithmetic operators

---
+++ ArithmeticOperatorsExample.java
@@ -0,0 +1,29 @@
+// This program demonstrates the use of arithmetic operators in Java
+
+public class ArithmeticOperatorsExample {
+    public static void main(String[] args) {
+        // Declare variables
+        int num1 = 10;
+        int num2 = 5;
+
+        // Addition
+        int sum = num1 + num2;
+        System.out.println("Addition: " + num1 + " + " + num2 + " = " + sum);
+
+        // Subtraction
+        int difference = num1 - num2;
+        System.out.println("Subtraction: " + num1 + " - " + num2 + " = " + difference);
+
+        // Multiplication
+        int product = num1 * num2;
+        System.out.println("Multiplication: " + num1 + " * " + num2 + " = " + product);
+
+        // Division
+        int quotient = num1 / num2;
+        System.out.println("Division: " + num1 + " / " + num2 + " = " + quotient);
+
+        // Modulus
+        int remainder = num1 % num2;
+        System.out.println("Modulus: " + num1 + " % " + num2 + " = " + remainder);
+    }
+}
