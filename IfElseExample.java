6851c27d880809094a162e99871e2ee53bfd8988
Utkarsh Bhutada <124357090+Utkarsh0130@users.noreply.github.com>
2024-07-22 19:33:24 +0530

    Day-3
    Understanding how conditional statements work in java

---
+++ IfElseExample.java
@@ -0,0 +1,14 @@
+// This program demonstrates the use of if-else statements in Java
+
+public class IfElseExample {
+    public static void main(String[] args) {
+        int number = 10;
+
+        // Check if number is positive
+        if (number > 0) {
+            System.out.println(number + " is positive.");
+        } else {
+            System.out.println(number + " is not positive.");
+        }
+    }
+}
