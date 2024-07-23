5cb2723c61b892fa409bc9745c24aa98d9be21cf
Utkarsh Bhutada <124357090+Utkarsh0130@users.noreply.github.com>
2024-07-23 20:20:27 +0530

    Day-4
    This is program showing working of for loop

---
+++ ForLoopExample.java
@@ -0,0 +1,10 @@
+// This program demonstrates the use of for loop in Java
+
+public class ForLoopExample {
+    public static void main(String[] args) {
+        // Using for loop to print numbers from 1 to 5
+        for (int count = 1; count <= 5; count++) {
+            System.out.println("Count: " + count);
+        }
+    }
+}
