e1a9c0a72e9237e50e089b984a086bd8da42c5cd
Utkarsh Bhutada <124357090+Utkarsh0130@users.noreply.github.com>
2024-07-23 20:18:41 +0530

    Day-4
    This is program which shows working of do while loop

---
+++ DoWhileLoopExample.java
@@ -0,0 +1,13 @@
+// This program demonstrates the use of do-while loop in Java
+
+public class DoWhileLoopExample {
+    public static void main(String[] args) {
+        int count = 1;
+
+        // Using do-while loop to print numbers from 1 to 5
+        do {
+            System.out.println("Count: " + count);
+            count++; // Incrementing the counter
+        } while (count <= 5);
+    }
+}
