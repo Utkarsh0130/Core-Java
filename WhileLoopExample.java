2f6af0601bc5c3132c1bd14758297c674f3ad6f2
Utkarsh Bhutada <124357090+Utkarsh0130@users.noreply.github.com>
2024-07-23 20:17:18 +0530

    Day-4
    This is the program of working of while loop

---
+++ WhileLoopExample.java
@@ -0,0 +1,13 @@
+// This program demonstrates the use of while loop in Java
+
+public class WhileLoopExample {
+    public static void main(String[] args) {
+        int count = 1;
+
+        // Using while loop to print numbers from 1 to 5
+        while (count <= 5) {
+            System.out.println("Count: " + count);
+            count++; // Incrementing the counter
+        }
+    }
+}
