3e3ccfa7eed7ce44852612b2ad881ac4b252249a
Utkarsh Bhutada <124357090+Utkarsh0130@users.noreply.github.com>
2024-07-30 21:08:11 +0530

    Day

---
+++ MethodOverridingExample.java
@@ -0,0 +1,24 @@
+// This program demonstrates method overriding in Java
+
+class Animal {
+    void sound() {
+        System.out.println("Animal makes a sound.");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Dog barks.");
+    }
+}
+
+public class MethodOverridingExample {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        animal.sound();
+
+        Dog dog = new Dog();
+        dog.sound();
+    }
+}
